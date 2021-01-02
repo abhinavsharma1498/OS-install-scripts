cf7f4fbb36d9604f6f80cd6fdcdf1e0a494598dc
Abhinav Sharma <abhisharma9784@gmail.com>
2021-01-02 17:29:19 +0530

    Script for manjaro

---
+++ Manjaro/manjaro.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#   System upgrade
+sudo pacman -Syyu --noconfirm
+
+#   AUR helper
+sudo pacman -S yay --noconfirm
+
+#   System tools
+sudo pacman -S caffeine --noconfirm
+
+#   Development dependencies
+sudo pacman -S base-devel clang code go docker npm --noconfirm
+yay spring-tool-suite 
+sudo npm install --global @angular/cli --noconfirm
+
+#   Virtualbox
+sudo pacman -S virtualbox
+
+#   Messaging
+sudo pacman -R thunderbird --noconfirm
+sudo pacman -S telegram-desktop discord --noconfirm
+
+#   Feature for SSD
+sudo systemctl enable fstrim.timer
